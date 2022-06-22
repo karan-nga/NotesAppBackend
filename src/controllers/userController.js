02e6901a5a2effc58f44c819745e19c82c9aae09
Karan <karannagra9924@gmail.com>
2022-06-22 17:22:29 +0530

    make it better

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 const userModel=require("../models/user");
-const bcrypt=require("bcrypt")
+const bcrypt=require("bcryptjs")
 const jwt=require("jsonwebtoken")
 const SECRET_KEY=process.env.SECRET_KEY;
 const signup= async(req,res) => { //register
